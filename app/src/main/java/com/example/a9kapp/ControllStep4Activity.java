981ab69362dfcc5c9e2792cefa3fc2b92f6d2f73
JoakimNAgornoff <joakim.nagornoff@live.se>
2019-11-14 14:28:11 +0100

    step4 activity klart

---
+++ app/src/main/java/com/example/a9kapp/ControllStep4Activity.java
@@ -0,0 +1,14 @@
+package com.example.a9kapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ControllStep4Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_controll_step4);
+    }
+}
